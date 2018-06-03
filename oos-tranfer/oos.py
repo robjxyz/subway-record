c17151cb48f6b2db11dad90130d72630c4343976
Robj <darnrascal@gmail.com>
2018-06-03 19:38:44 -0400

    Wrote a function to attach stop codes to the entrances file

--- oos-tranfer/oos.py
+++ oos-tranfer/oos.py
@@ -8,10 +8,32 @@ def pack(header, row):
         d[header[i]]=row[i]
     return d
 
+entrances = []
 with open("StationEntrances.csv") as f:
     reader = csv.reader(f)
     header = next(reader)
-    data = [pack(header,r) for r in reader]
-    print(len(data))
-    print(data[0])
+    entrances = [pack(header,r) for r in reader]
 
+stations = []
+with open("Stations.csv") as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    stations = [pack(header,r) for r in reader]
+
+print(len(entrances))
+print(entrances[0])
+
+print(len(stations))
+print(stations[0])
+
+def stationCode(e):
+    print(e['Station_Name'])
+    print(e['Station_Latitude'])
+    print(e['Station_Longitude'])
+    for s in stations:
+        if s['GTFS Latitude'] == e['Station_Latitude']:
+            if s['GTFS Longitude'] == e['Station_Longitude']:
+                return(s['GTFS Stop ID'])
+
+for ent in entrances:
+  print(stationCode(ent))
