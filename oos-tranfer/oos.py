576b95cc1cc63c801431d44d0b71b02cfc163543
robjxyz <robjxyz@github.com>
2018-06-05 10:47:50 -0400

    Adding ability to search between entrances and stations

--- oos-tranfer/oos.py
+++ oos-tranfer/oos.py
@@ -1,4 +1,4 @@
-import csv
+import csv,random,json#,requests
 
 #take a list of headers, and a row. 
 #return a dict {h[0]:r[0],h[1]:r[1]...}
@@ -7,12 +7,33 @@ def pack(header, row):
     for i in range(len(header)):
         d[header[i]]=row[i]
     return d
-
+#Given an original "Enrance" row, find the corresponding
+# station code using the gps coordinate match from "Stations"
 def stationCode(e):
     for s in stations:
         if s['GTFS Latitude'] == e['Station_Latitude']:
             if s['GTFS Longitude'] == e['Station_Longitude']:
                 return(s['GTFS Stop ID'])
+#Given a station row, find the corresponding Entrances, return as a list
+def getEnts(s):
+	ents = []
+	for e in entrances:
+		if e['Stop ID'] == s['GTFS Stop ID']:
+			ents.append(e)
+	return ents
+def gmapsDistance(s1,s2):
+	origin = '{0},{1}'.format(
+		s1['GTFS Latitude'],s1['GTFS Longitude'])
+	dest = '{0},{1}'.format(
+		s2['GTFS Latitude'],s2['GTFS Longitude'])
+	key = ''
+	units = 'imperial'
+	mode = 'walking'
+	url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+	request = '{0}units={1}&mode={2}&origins={3}&destinations={4}&key={5}'.format(
+		url,units,mode,origin,dest,key)
+	print(request)
+
 stations = []
 with open("Stations.csv") as f:
     reader = csv.reader(f)
@@ -24,14 +45,12 @@ with open("StationEntrances.csv") as f:
     header = next(reader)
     entrances = [pack(header,r) for r in reader]
 for ent in entrances:
-	ent['Stop_Code']=stationCode(ent)
+	ent['Stop ID']=stationCode(ent)
 
-print(len(entrances))
-print(entrances[0])
+stopone = random.choice(stations)
+stoptwo = random.choice(stations)
 
-print(len(stations))
-print(stations[0])
-
-
+gmapsDistance(stopone,stoptwo)
 #for ent in entrances:
+
 #  print(stationCode(ent))
