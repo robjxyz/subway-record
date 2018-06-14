68465cbd0e0ed784e97461a9f59677244075b6d5
robjxyz <robjxyz@github.com>
2018-06-14 19:06:36 -0400

    Added more to lines file

--- oos-tranfer/linesDefs.py
+++ oos-tranfer/linesDefs.py
@@ -75,7 +75,84 @@ def fultonM((code,stop)):
 def fultonS((code,stop)):
 	return code=='A' and 63<=stop<=65
 def rockN((code,stop)):
-	return code==
+	return code=='H' and 1<=stop<=4
+def rockM((code,stop)):
+	return code=='H' and 6<=stop<=11
+def rockS((code,stop)):
+	return code=='H' and 12<=stop<=15
+
+#D train
+def concourse((code,stop)):
+	return code=='D' and 3<=stop<=12
+def six((code,stop)):
+	if code=='D' and 15<=stop<=21: return True
+	if code=='A' and stop==32: return True
+	return False
+def fourN((code,stop)):
+	return code=='R' and 31<=stop<=36
+def west((code,stop)):
+	if code=='B' and 12<=stop<=23: return True
+	if code=='D' and stop==43: return True
+	return False
+
+#E train
+def archer((code,stop)):
+	return code=='G' and 5<=stop<=6
+def queens((code,stop)):
+	if code=='F' and 5<=stop<=7: return True
+	if code=='G' and 8<=stop<=20: return True
+	return False
+def fiftythird((code,stop)):
+	return code=='F' and 9<=stop<=12
+
+#F train
+def hill((code,stop)):
+	return code=='F' and 1<=stop<=4
+def sixtythird((code,stop)):
+	return code=='B' and 4<=stop<=10
+def les((code,stop)):
+	return code=='F' and 14<=stop<=18
+def culverN((code,stop)):
+	return code=='F' and 20<=stop<=39
+def culverS((code,stop)):
+	return code=='D' and 42<=stop<=43
+
+#G train
+def cross((code,stop)):
+	return code=='G' and 22<=stop<=36
+
+#J train
+def jamaicaN((code,stop)):
+	return code=='J' and 12<=stop<=31
+def jamaicaS((code,stop)):
+	return code=='M' and 11<=stop<=23
+
+#L train
+def canarsie((code,stop)):
+	return code=='L'
+
+#M train
+def myrtle((code,stop)):
+	return code=='M' and 1<=stop<=10
+
+#N train
+def astoria((code,stop)):
+	return code=='R' and 1<=stop<=8
+def fiftyninth((code,stop)):
+	return code=='R' and 11<=stop<=13
+def bwy((code,stop)):
+	return code=='R' and 14<=stop<=23
+def fourM((code,stop)):
+	return code=='R' and 39<=stop<=41
+def sea((code,stop)):
+	if code=='N' and 2<=stop<=10: return True
+	if code=='D' and stop==43: return True
+	return False
+
+#Q train
+def second((code,stop)):
+	return code=='Q' and 3<=stop<=5
+
 
 
 #Tests
